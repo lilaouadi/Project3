360fa683a4f32389a0f003ea71029cf24eb65a71
Lila Ouadi <lilaouadi@Lilas-Air.attlocal.net>
2020-03-21 09:26:00 -0700

    added nav bar

--- politics/client/src/components/Nav/index.js
+++ politics/client/src/components/Nav/index.js
@@ -1,29 +1,21 @@
-import React from "react";
-import {Navbar, Icon, NavItem} from "react-materialize";
-
-function Nav() {
+import React from 'react';
+import M from 'materialize-css';
+const Nav = props => {
   return (
-    <Navbar
-    alignLinks="right"
-    brand={<a className="brand-logo" href="#">Logo</a>}
-    menuIcon={<Icon>menu</Icon>}
-    options={{
-      draggable: true,
-      edge: 'left',
-      inDuration: 250,
-      onCloseEnd: null,
-      onCloseStart: null,
-      onOpenEnd: null,
-      onOpenStart: null,
-      outDuration: 200,
-      preventScrolling: true
-    }}
-    search
-  >
-
-  </Navbar>
-   
+    <div>
+      <nav>
+        <div class="nav-wrapper">
+          <a href="#" class="brand-logo">Logo</a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">JavaScript</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
-}
+};
+
 
 export default Nav;
