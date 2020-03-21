565bcf3fd365681ef9706c3afda9b29c1f7b2996
Ashish Kafle <jptsam@ashishs-air.attlocal.net>
2020-03-21 11:23:13 -0700

    created login page

--- politics/client/src/components/Card/index.js
+++ politics/client/src/components/Card/index.js
@@ -14,7 +14,7 @@ const CardComponent = props => {
                     <a key="1" href="#">This is a Link</a>
                 ]}
                 closeIcon={<Icon>close</Icon>}
-                header={<CardTitle image="https://materializecss.com/images/sample-1.jpg">Card Title</CardTitle>}
+                header={<CardTitle image="https://materializecss.com/images/sample-1.jpg">Hello</CardTitle>}
                 revealIcon={<Icon>more_vert</Icon>}
                 >
                 Here is the standard card with an image thumbnail.
