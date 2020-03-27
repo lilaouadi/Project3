8072cba5b760ba690a3c62150cf7817ea00375ab
jimmy nagles <jimmynagles@gmail.com>
2020-03-26 20:23:49 -0700

    dropdown for mobile

--- politics/client/src/components/Nav/Navbar.js
+++ politics/client/src/components/Nav/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Icon} from 'react-materialize';
+import {Icon,Dropdown,Button,Divider} from 'react-materialize';
 
 
 
@@ -12,12 +12,20 @@ import {Icon} from 'react-materialize';
                 <nav>
 
                     <div className="nav-wrapper black">
+                    <Dropdown className="black " id="Dropdown-5" trigger={<Button className="btn-flat white-text hide-on-med-and-up " node="button"><Icon small className="left">dehaze</Icon></Button>}>
+                        <a href="/SignUP"><Icon small className="left">add</Icon>Sign Up</a>
+                        <a href="/SignIn"><Icon small className='left'>account_circle</Icon>Sign In</a>         
+                        <a href="/">Details</a>
+                        <a href="/">About</a>
+
+                    </Dropdown>
                     <a href="/" className="brand-logo center">MP list</a>
+
                     
                     <ul id="nav-mobile" className="right hide-on-med-and-down white-text">
                       
-                        <li><a href="collapsible.html">About</a></li>
                         <li><a href="/">Details</a></li>
+                        <li><a href="collapsible.html">About</a></li>
                         
                         
                     </ul>
@@ -28,7 +36,8 @@ import {Icon} from 'react-materialize';
                       <li><a href="/SignUP"><Icon small className="left">add</Icon>Sign Up</a></li>
                       <li><a href="/SignIn">Sign In<Icon small className='left'>account_circle</Icon></a></li>
                       
-                  </ul>
+                     </ul>
+
                   </div>
                 </nav>
 
