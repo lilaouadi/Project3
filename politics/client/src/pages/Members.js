281744843f7583cdddd77e00da8c96a7884362b0
Lila Ouadi <lilaouadi@Lilas-Air.attlocal.net>
2020-03-26 19:43:46 -0700

    Message

--- politics/client/src/pages/Members.js
+++ politics/client/src/pages/Members.js
@@ -1,16 +1,9 @@
 import React,{Component} from "react";
 import API from "../utils/API";
-<<<<<<< HEAD
-import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../components/Grid";
-import { List, ListItem } from "../components/List";
-import Card from '../components/Card';
-=======
 import MemberCard from "../components/Card/MemberCard"
 import Searchbar from "../components/Nav/SearchBar";
 import {  Container, Row, Col } from 'react-materialize';
 
->>>>>>> 6eecb601d76eecc7ae3565c46c74e48ac18feb65
 
 
 class Members extends Component {
@@ -64,39 +57,6 @@ class Members extends Component {
 
   render() {
     return (
-<<<<<<< HEAD
-      <Container fluid>
-        <Row>
-          <Col size="md-3"></Col>
-          <Col size="md-6 sm-12">
-            <Jumbotron>
-              <h1>Members Of The 9th Parliament of Zimbabwe</h1>
-            </Jumbotron>
-            {this.state.members.length ? (
-              <List>
-                {this.state.members.map(member => (
-                  <ListItem key={member._id}>
-                    <Link to={"/members/" + member._id}>
-                      <strong>
-                        {member.constituency} constituency is represented by {member.lastName}
-                      </strong>
-                    </Link>
-                    <DeleteBtn onClick={() => this.deleteMember(member._id)} />
-                  </ListItem>
-             
-                ))}
-              </List>
-            ) : (
-              <div>
-                  <h3>No Results to Display</h3>
-                <Card />
-              </div>
-                
-              )}
-          </Col>
-        </Row>
-      </Container>
-=======
    
        <div>
 
@@ -133,7 +93,6 @@ class Members extends Component {
 
        
              </div>
->>>>>>> 6eecb601d76eecc7ae3565c46c74e48ac18feb65
     );
   }
 }
