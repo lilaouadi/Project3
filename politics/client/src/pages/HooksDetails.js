253f71108b8b6b6cddc28219f07baf9e0c9ada72
jimmy nagles <jimmynagles@gmail.com>
2020-08-11 12:08:18 -0700

    details to hooks

--- politics/client/src/pages/HooksDetails.js
+++ politics/client/src/pages/HooksDetails.js
@@ -16,8 +16,10 @@ import { MemberContext } from "../context/MembersContext";
 
 function HooksDetails(props) {
   const { members } = useContext(MemberContext);
-
+  //variable for member
   const member = members.find((member) => member._id === id);
+
+  const [match, setmatch] = useState({});
 
   const id = props.match.params.id;
   console.log("id", id);
@@ -34,16 +36,34 @@ function HooksDetails(props) {
     members.map((x, index) => {
       if (x._id === id) {
         console.log("Found it", x);
+        console.log("x._id", x._id);
+        console.log("x._id", x.lastName);
+
+        console.log("x._id", x.constituency);
+        console.log("x.imageHref", x.imageHref);
+        console.log("x.bio", x.bio);
+        console.log("x.", x.party);
+        setmatch(x);
       }
     });
   };
 
+  {
+  }
   return (
     <div>
       {/* tittle container */}
       <Container>
         <Card className="black white-text center">
           {console.log("members", members)}
+        </Card>
+      </Container>
+
+      <Container>
+        <Card className="black white-text center">
+          <h3>
+            {match.constituency} constituency is represented by {match.lastName}
+          </h3>
         </Card>
       </Container>
     </div>
