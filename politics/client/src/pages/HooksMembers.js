4bc1ef02bfa0ffbfba31810f1b75f4c76678fd5e
jimmy nagles <jimmynagles@gmail.com>
2020-08-11 11:14:00 -0700

    members page done

--- politics/client/src/pages/HooksMembers.js
+++ politics/client/src/pages/HooksMembers.js
@@ -3,7 +3,12 @@ import React, { useContext, useEffect, useState } from "react";
 import API from "../utils/API";
 
 import { MemberContext } from "../context/MembersContext";
+import MemberCard from "../components/Card/MemberCard"
 import Searchbar from "../components/Nav/SearchBar";
+import {  Container, Row, Col } from 'react-materialize';
+import { Link } from "react-router-dom";
+import MPimage from "../img/MPimage.png"
+
 
 function HooksMembers() {
   const { members, setmembers } = useContext(MemberContext);
@@ -26,6 +31,25 @@ function HooksMembers() {
     <div>
       <Searchbar></Searchbar>
       {console.log("members", members)}
+
+      {members.length ? (
+        <Row>
+          {members.map((member) => (
+            <MemberCard
+              key={member._id}
+              image={MPimage}
+              lastName={member.lastName}
+              constituency={member.constituency}
+              bio={member.bio}
+              search={<Link to={"/members/" + member._id}>more details</Link>}
+            />
+          ))}
+        </Row>
+      ) : (
+        <Row>
+          <h3 className="center">loading...</h3>
+        </Row>
+      )}
     </div>
   );
 }
