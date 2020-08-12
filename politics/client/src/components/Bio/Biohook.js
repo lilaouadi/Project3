333016e61fe8e6c756254147d997ea02170019a3
jimmy nagles <jimmynagles@gmail.com>
2020-08-12 08:12:20 -0700

    details refactor done

---
+++ politics/client/src/components/Bio/Biohook.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Biohook = () => {
+  return <div></div>;
+};
+
+export default Biohook;
--- politics/client/src/pages/HooksDetails.js
+++ politics/client/src/pages/HooksDetails.js
@@ -21,44 +21,41 @@ function HooksDetails(props) {
 
   const [match, setmatch] = useState({});
 
+  const [fix, setfix] = useState(false);
+
+  const [bio, setbio] = useState("");
   const id = props.match.params.id;
   console.log("id", id);
 
   console.log("member", member);
 
   useEffect(() => {
-    if (member === undefined) {
+    if (fix === false) {
       findMember(id);
+      setfix(true);
     }
-  }, []);
+  });
+  //takes in the id and maps throught the array of members
+  //until it finds one that matches
+  //updates state to the member we are looking for
 
   const findMember = (id) => {
-    members.map((x, index) => {
+    members.map((x) => {
       if (x._id === id) {
-        console.log("Found it", x);
-        console.log("x._id", x._id);
-        console.log("x._id", x.lastName);
-
-        console.log("x._id", x.constituency);
-        console.log("x.imageHref", x.imageHref);
         console.log("x.bio", x.bio);
-        console.log("x.", x.party);
+
+        setbio(x.bio);
         setmatch(x);
+
+        console.log("bio", bio);
+        console.log("match", match);
       }
     });
   };
 
-  {
-  }
   return (
     <div>
       {/* tittle container */}
-      <Container>
-        <Card className="black white-text center">
-          {console.log("members", members)}
-        </Card>
-      </Container>
-
       <Container>
         <Card className="black white-text center">
           <h3>
@@ -66,6 +63,43 @@ function HooksDetails(props) {
           </h3>
         </Card>
       </Container>
+
+      {/* Row for everything */}
+
+      <Row>
+        {/* Col for member info */}
+
+        <Col m={6} s={12}>
+          <Card
+            className="black white-text"
+            header={<CardTitle image={match.imageHref}></CardTitle>}
+          ></Card>
+        </Col>
+
+        {/* col for member Bio */}
+        <Col className="l6">
+          <Card className="black white-text">
+            <h1>Bio</h1>
+            <h5>{bio.toString().trim().split(":")}</h5>
+          </Card>
+        </Col>
+
+        {/* col for updating the bio */}
+        <Col className="l6">
+          <Card className="black white-text">
+            <article>
+              <h1 className="white-text">Would you like to update bio?</h1>
+            </article>
+          </Card>
+        </Col>
+      </Row>
+
+      {/* row */}
+      <Row>
+        <Col size="md-2">
+          <Link to="/">← Back to Representatives</Link>
+        </Col>
+      </Row>
     </div>
   );
 }
